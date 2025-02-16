3d16c36bd60606402ae2b44d2aef3d9e55a23517
Boazalt <boazlangat09@gmail.com>
2025-02-16 10:43:42 +0300

    Velocity

--- cli-examples/speed-test.js
+++ cli-examples/speed-test.js
@@ -1,4 +1,4 @@
-var token = 'Replace with your own token';
+ivar token = 'Replace with your own token';
 
 console.log('Starting bot...');
 
@@ -35,3 +35,56 @@ while (true) {
 
   sleep(1) // Prevent max sell alert because of trading too fast
 }
+<block type="controls_if" id="check_digit_9">
+  <mutation else="1"></mutation>
+  <value name="IF0">
+    <block type="logic_compare" id="compare_digit_9">
+      <field name="OP">EQ</field>
+      <value name="A">
+        <block type="math_number" id="digit_9">
+          <field name="NUM">9</field>
+        </block>
+      </value>
+      <value name="B">
+        <block type="variables_get" id="detected_digit">
+          <field name="VAR">digit</field>
+        </block>
+      </value>
+    </block>
+  </value>
+  <statement name="DO0">
+    <block type="text_print" id="execute_digit_9">
+      <value name="TEXT">
+        <shadow type="text" id="text_to_print">
+          <field name="TEXT">Executing digit 9</field>
+        </shadow>
+      </value>
+    </block>
+  </statement>
+</block><block type="controls_if" id="check_digit_9">
+  <mutation else="1"></mutation>
+  <value name="IF0">
+    <block type="logic_compare" id="compare_digit_9">
+      <field name="OP">EQ</field>
+      <value name="A">
+        <block type="math_number" id="digit_9">
+          <field name="NUM">9</field>
+        </block>
+      </value>
+      <value name="B">
+        <block type="variables_get" id="detected_digit">
+          <field name="VAR">digit</field>
+        </block>
+      </value>
+    </block>
+  </value>
+  <statement name="DO0">
+    <block type="text_print" id="execute_digit_9">
+      <value name="TEXT">
+        <shadow type="text" id="text_to_print">
+          <field name="TEXT">Executing digit 9</field>
+        </shadow>
+      </value>
+    </block>
+  </statement>
+</block>
